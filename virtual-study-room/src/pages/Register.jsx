7b3a027b50cfe00d65cf840a0ddfe5327318f77c
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-08-12 19:19:52 +0530

    added login and register pages

--- virtual-study-room/src/pages/Register.jsx
+++ virtual-study-room/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from "styled-components";
 
 
-export default function Login() {
+export default function Register() {
   return (
     <>
     <FormContainer>
@@ -15,40 +15,25 @@ export default function Login() {
       </div>
       <div class="login-box">
         <form class="login-form" action="index.html">
-          <h3 class="login-head"><i class="bi bi-person me-2"></i>SIGN IN</h3>
+          <h3 class="login-head"><i class="bi bi-person me-2"></i>SIGN UP</h3>
           <div class="mb-3">
             <label class="form-label">USERNAME</label>
-            <input class="form-control" type="text" placeholder="Email" autofocus />
+            <input class="form-control" type="text" placeholder="username" autofocus />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input class="form-control" type="email" placeholder="email" />
           </div>
           <div class="mb-3">
             <label class="form-label">PASSWORD</label>
             <input class="form-control" type="password" placeholder="Password" />
           </div>
           <div class="mb-3">
-            <div class="utility">
-              <div class="form-check">
-                <label class="form-check-label">
-                  <input class="form-check-input" type="checkbox" /><span class="label-text" >Stay Signed in</span>
-                </label>
-              </div>
-              <p class="semibold-text mb-2"><a href="#" data-toggle="flip">Forgot Password ?</a></p>
-            </div>
+            <label class="form-label">Phone</label>
+            <input class="form-control" type="text" placeholder="Phone" />
           </div>
           <div class="mb-3 btn-container d-grid">
-            <button class="btn btn-primary btn-block"><i class="bi bi-box-arrow-in-right me-2 fs-5"></i>SIGN IN</button>
-          </div>
-        </form>
-        <form class="forget-form" action="index.html">
-          <h3 class="login-head"><i class="bi bi-person-lock me-2"></i>Forgot Password ?</h3>
-          <div class="mb-3">
-            <label class="form-label">EMAIL</label>
-            <input class="form-control" type="text" placeholder="Email" />
-          </div>
-          <div class="mb-3 btn-container d-grid">
-            <button class="btn btn-primary btn-block"><i class="bi bi-unlock me-2 fs-5"></i>RESET</button>
-          </div>
-          <div class="mb-3 mt-3">
-            <p class="semibold-text mb-0"><a href="#" data-toggle="flip"><i class="bi bi-chevron-left me-1"></i> Back to Login</a></p>
+            <button class="btn btn-primary btn-block"><i class="bi bi-box-arrow-in-right me-2 fs-5"></i>SIGN UP</button>
           </div>
         </form>
       </div>
