b4ed41bc10cf5a77c70b030fa8a0e5b42b1cc55d
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-08-12 18:18:28 +0530

    added files

--- virtual-study-room/src/App.js
+++ virtual-study-room/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SetAvatar from "./components/SetAvatar";
-import Chat from "./pages/Chat";
+// import SetAvatar from "./components/SetAvatar";
+// import Chat from "./pages/Chat";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        {/* <Route path="/register" element={<Register />} /> */}
         <Route path="/login" element={<Login />} />
-        <Route path="/setAvatar" element={<SetAvatar />} />
-        <Route path="/" element={<Chat />} />
+        {/* <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} /> */}
       </Routes>
     </BrowserRouter>
   );
